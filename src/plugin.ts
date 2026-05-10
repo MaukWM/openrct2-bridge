b4379e3bc1ae78d4c86d9727d080a6b2a34c9690
MaukWM <mauk@elnino.tech>
2026-05-10 23:52:43 +0200

    :bookmark: Bump version for new release.

--- src/plugin.ts
+++ src/plugin.ts
@@ -10,7 +10,7 @@ import * as world from "./world";
 var BASE_PORT = 20020;
 var MAX_PORT = 65535;
 var PLUGIN_NAME = "openrct2-bridge";
-var PLUGIN_VERSION = "1.7.0";
+var PLUGIN_VERSION = "1.8.0";
 
 // ── Built-in endpoints ───────────────────────────────────────────────
 
